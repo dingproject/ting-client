fc6228912662436db1a3a60a54718a268aec4519
Kasper Garnæs <kasper@garnaes.dk>
2009-04-30 13:36:53 +0200

    Added unit tests for facets and number of results

--- test/TingClientLiveZfTest.php
+++ test/TingClientLiveZfTest.php
@@ -12,22 +12,62 @@ require_once 'Zend/Http/Client.php';
 class TingClientLiveTest extends UnitTestCase {
 	
 	private $baseUrl = 'http://didicas.dbc.dk/opensearch/';
+
+	/**
+	 * @var TingClient
+	 */
+	private $client;
 	
-	function testRequest()
+	function __construct()
 	{
-		//End to end test
 		$requestAdapter = new TingClientZfHttpRequestAdapter(	new Zend_Http_Client(),
 																													new TingClientHttpRequestFactory($this->baseUrl));
 		$responseAdapter = new TingClientJsonResponseAdapter();
 
-		$client = new TingClient($requestAdapter, $responseAdapter);
-
+		$this->client = new TingClient($requestAdapter, $responseAdapter);
+		
+	}
+	
+	function testRequest()
+	{
+		//End to end test
 		$searchRequest = new TingClientSearchRequest('dc.title:danmark');
-		$searchRequest->setOutput('json');
+		$searchRequest->setOutput('json');		
+		$searchResult = $this->client->search($searchRequest);
 		
-		$searchResult = $client->search($searchRequest);
+		$this->assertNoErrors('Search should not throw errors');
+	}
+
+	function testNumResults()
+	{
+		$searchRequest = new TingClientSearchRequest('dc.title:danmark');
+		$searchRequest->setNumResults(1);
+		$searchRequest->setOutput('json');		
+		$searchResult = $this->client->search($searchRequest);
+
+		$this->assertNoErrors('Search should not throw errors');
 		
-		$this->assertNoErrors('Search should not throw errors!');
+		$this->assertEqual(sizeof($searchResult->getRecords()), 1, 'Returned number of results does not match requested number');						
+	}
+	
+	function testFacets()
+	{
+		$facetName = 'dc.title';
+		
+		$searchRequest = new TingClientSearchRequest('dc.title:danmark');
+		$searchRequest->setFacets('dc.title');
+		$searchRequest->setOutput('json');		
+		$searchResult = $this->client->search($searchRequest);
+
+		$this->assertNoErrors('Search should not throw errors');
+		
+		$searchFacetFound = false;
+		$facetResults = $searchResult->getFacets();
+		foreach ($facetResults as $facet)
+		{
+			$searchFacetFound |= $facet->getName() == $facetName;
+		}
+		$this->assertTrue($searchFacetFound, 'Expected facet used in search was not part of search result');						
 	}
 
 }
