6a7b837565db00f3746963b1d38ac35d10deb5f7
Kasper Garnæs <kasper@garnaes.dk>
2009-05-14 15:22:44 +0200

    Added initial version of logger for Drupal watchdog

---
+++ lib/log/TingClientDrupalWatchDogLogger.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once dirname(__FILE__).'/TingClientLogger.php';
+
+/**
+ * Ting logger wrapper for the Drupal watchdog API.
+ * 
+ * @see http://api.drupal.org/api/function/watchdog/
+ */
+class TingClientDrupalWatchDogLogger extends TingClientLogger 
+{
+	
+	public function doLog($message, $severity)
+	{
+		watchdog(	'TingClient', $message, null, 
+							constant('WATCHDOG_'.$severity), 
+							'http://'.$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
+	}	
+	
+}
