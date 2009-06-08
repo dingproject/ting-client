85d186bb5aa4d303be142877d398ca42f0c8cac8
kasperg <kasper@headinsky.dk>
2009-06-08 16:29:12 +0200

    Removed obsolete import from JSON response adapter

--- lib/adapter/response/json/TingClientJsonResponseAdapter.php
+++ lib/adapter/response/json/TingClientJsonResponseAdapter.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once dirname(__FILE__).'/../TingClientResponseAdapter.php';
-require_once dirname(__FILE__).'/../../../search/data/TingClientRecordDataFactory.php';
 require_once dirname(__FILE__).'/../../../log/TingClientLogger.php';
 
 class TingClientJsonResponseAdapter implements TingClientResponseAdapter 
