698ab4521192b30d23818ec2b97d602bed78d6ba
kasperg <kasper@headinsky.dk>
2010-03-17 13:19:08 +0100

    Use contains instead of equality when searching based on local ids

--- lib/request/rest-json/RestJsonTingClientObjectRequest.php
+++ lib/request/rest-json/RestJsonTingClientObjectRequest.php
@@ -46,23 +46,19 @@ class RestJsonTingClientObjectRequest extends RestJsonTingClientRequest
 		{
 			$request = new RestJsonTingClientSearchRequest($this->baseUrl);
 			
-			//determine which id to use and the corresponding index
-			$ids = array(	'objectId' => 'rec.id',
-							      'localId' => 'rec.id');
-			foreach ($ids as $attribute => $index)
-			{
-				$id = call_user_func(array($this, 'get'.ucfirst($attribute)));
-				if ($id)
-				{	
-					$request->setQuery($index.'='.$id);
-					break;
-				}
+			//Determine which id to use and the corresponding index
+			if ($objectId = $this->getObjectId()) {
+				$request->setQuery('rec.id='.$objectId);				
+			} elseif ($localId = $this->getLocalId()) {
+				//Use contains (:) instead of equality (=) as local id is not a complete value
+				//TODO: Update to support complete faust numbers (local id and owner id) 
+				$request->setQuery('rec.id:'.$localId);
 			}
 			
 			$request->setNumResults(1);
 			
 			$searchResult = $request->execute($adapter);
-			//var_dump($searchResult);
+			
 			return $searchResult->collections[0]->objects[0];
 		}
 
