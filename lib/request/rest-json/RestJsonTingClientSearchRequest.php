e7fe5ed4df64407319211ce835d39c514ca9c61e
Thomas Fini Hansen <xen@xen.dk>
2011-02-24 11:11:31 +0100

    Removed same-library-check from relations.

--- lib/request/rest-json/RestJsonTingClientSearchRequest.php
+++ lib/request/rest-json/RestJsonTingClientSearchRequest.php
@@ -160,17 +160,6 @@ class RestJsonTingClientSearchRequest extends RestJsonTingClientRequest
 
       if (isset($objectData->relations)) {
         foreach ($objectData->relations->relation as $relation) {
-          // Remove relations that's not from the local agency.
-          $id = $relation->relationObject->object->record->identifier;
-          if ($id[0]->{'@'} == 'ac') {
-            list($l, $o) = explode('|', $id[0]->{'$'});
-            if (!$o || $o != $object->ownerId) {
-              continue;
-            }
-          }
-          else {
-            continue;
-          }
           $relation_object = $this->generateObject($relation->relationObject->object, $namespaces);
           $relation_object->relationType = $relation->relationType->{'$'};
           $relation_object->relationUri = $relation->relationUri->{'$'};
