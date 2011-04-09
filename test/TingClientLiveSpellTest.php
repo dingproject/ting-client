ff13a31f5d3863bc4723d79fbc1b1669a74ef063
Mikkel Hoegh <mikkel@hoegh.org>
2011-04-09 23:50:28 +0200

    This almost passes now.
    It actually delivers the correct number of results, but nested in an array.
    I need to figure out how to unpack that without changing the API.

--- test/TingClientLiveSpellTest.php
+++ test/TingClientLiveSpellTest.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once dirname(__FILE__) . '/TingClientLiveZfTest.php';
+require_once dirname(__FILE__) . '/TingClientLiveSoapTest.php';
 
-class TingClientLiveSpellTest extends TingClientLiveZfTest
+class TingClientLiveSpellTest extends TingClientLiveSoapTest
 {
 
 	function testSpelling()
 	{
 		$request = $this->requestFactory->getSpellRequest();
-		$request->setWord('tilyke');
+		$request->setWord('terasse'); // Should be 'terrasse', but gives lots of other fun suggestions.
 		$request->setNumResults(3);
 		$suggestions = $this->client->execute($request);
 
