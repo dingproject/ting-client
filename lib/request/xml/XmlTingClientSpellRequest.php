a94e4bf66543661e551b8c149e3f0d9b5a062b96
Mikkel Hoegh <mikkel@hoegh.org>
2011-04-09 23:49:15 +0200

    Configuring SoapRequest for Open Spell.

--- lib/request/xml/XmlTingClientSpellRequest.php
+++ lib/request/xml/XmlTingClientSpellRequest.php
@@ -10,6 +10,38 @@ class XmlTingClientSpellRequest extends XmlTingClientRequest
     protected $word;
     protected $numResults;
 
+    protected function getSoapRequest()
+    {
+      $soapRequest = new TingClientSoapRequest();
+      $soapRequest->setWsdlUrl($this->wsdlUrl);
+      $soapRequest->setParameter('action', 'openSpell');
+      $soapRequest->setParameter('format', 'dkabm');
+
+      // TODO: Figure out what this does. It's required in the SOAP
+      // implementation, but not used in the REST implementation.
+      $soapRequest->setParameter('filter', '');
+
+      // TODO: This should be configurable somewhere, even though it's not an
+      // option in the REST implementation.
+      $soapRequest->setParameter('language', 'da');
+
+      $methodParameterMap = array(
+        'word' => 'word',
+        'numResults' => 'number',
+      );
+
+      foreach ($methodParameterMap as $method => $parameter)
+      {
+        $getter = 'get'.ucfirst($method);
+        if ($value = $this->$getter())
+        {
+          $soapRequest->setParameter($parameter, $value);
+        }
+      }
+
+      return $soapRequest;
+    }
+
     public function getWord()
     {
       return $this->word;
