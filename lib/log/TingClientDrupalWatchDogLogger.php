c196d3316a8d8cf4e2caecbae8130e9444ea64e7
Mikkel Hoegh <mikkel@hoegh.org>
2011-04-26 23:34:00 +0200

    Code style, whitespace.

--- lib/log/TingClientDrupalWatchDogLogger.php
+++ lib/log/TingClientDrupalWatchDogLogger.php
@@ -4,17 +4,14 @@ require_once dirname(__FILE__).'/TingClientLogger.php';
 
 /**
  * Ting logger wrapper for the Drupal watchdog API.
- * 
+ *
  * @see http://api.drupal.org/api/function/watchdog/
  */
-class TingClientDrupalWatchDogLogger extends TingClientLogger 
-{
-	
-	public function doLog($message, $severity)
-	{
-		watchdog(	'ting client', $message, null, 
-							constant('WATCHDOG_'.$severity), 
-							'http://'.$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
-	}	
-	
+class TingClientDrupalWatchDogLogger extends TingClientLogger {
+  public function doLog($message, $severity) {
+    watchdog('ting client', $message, array(),
+             constant('WATCHDOG_' . $severity),
+             'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+  }
 }
+
--- lib/log/TingClientLogger.php
+++ lib/log/TingClientLogger.php
@@ -1,37 +1,35 @@
 <?php
 
 /**
- * Base logger class which can be injected into the Ting client. 
+ * Base logger class which can be injected into the Ting client.
  */
 abstract class TingClientLogger {
-	
-	const EMERGENCY = 'EMERGENCY';
-	const ALERT = 'ALERT';
-	const CRITICAL = 'CRITICAL';
-	const ERROR = 'ERROR';
-	const WARNING = 'WARNING';
-	const NOTICE = 'NOTICE';
-	const INFO = 'INFO';
-	const DEBUG = 'DEBUG';
 
-	static public $levels = array(self::EMERGENCY, self::ALERT, self::CRITICAL,	self::ERROR, 
-																self::WARNING,	self::NOTICE,	self::INFO,	self::DEBUG);
-	
+  const EMERGENCY = 'EMERGENCY';
+  const ALERT = 'ALERT';
+  const CRITICAL = 'CRITICAL';
+  const ERROR = 'ERROR';
+  const WARNING = 'WARNING';
+  const NOTICE = 'NOTICE';
+  const INFO = 'INFO';
+  const DEBUG = 'DEBUG';
+
+  static public $levels = array(self::EMERGENCY, self::ALERT, self::CRITICAL, self::ERROR,
+                                self::WARNING,  self::NOTICE, self::INFO, self::DEBUG);
+
   /**
    * Log a message.
    *
    * @param string $message The message to log
    * @param string $severity The severity of the message
    */
-	public function log($message, $severity = self::INFO)
-	{
-		if (!in_array($severity, self::$levels))
-		{
-			throw new TingClientException('Unsupported severity: '.$severity);
-		}
-		$this->doLog($message, $severity);
-	}
-	
-	abstract protected function doLog($message, $severity);
-	
+  public function log($message, $severity = self::INFO) {
+    if (!in_array($severity, self::$levels)) {
+      throw new TingClientException('Unsupported severity: '.$severity);
+    }
+    $this->doLog($message, $severity);
+  }
+
+  abstract protected function doLog($message, $severity);
 }
+
